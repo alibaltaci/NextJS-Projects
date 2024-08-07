03d84a0d501922820dbfdd908db0d296786895c9
ali baltaci <alibaltaci13@gmail.com>
2024-08-07 20:04:45 +0300

    update the news compnent

--- x-clone/next.config.mjs
+++ x-clone/next.config.mjs
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+
+    images:{
+        domains:[
+            'image.cnbcfm.com', 
+            "a57.foxnews.com", 
+            "cdn.vox-cdn.com",
+            "cdn.cnn.com",
+            "www.reuters.com",
+            "storage.googleapis.com",
+            "images.axios.com",
+            "i.kinja-img.com",
+            "images.wsj.net",
+            "content.fortune.com",
+            "i.ytimg.com",
+            "www.hollywoodreporter.com",
+            "reports.mortgagenewsdaily.com",
+            "bitcoinmagazine.com",
+            "images.barrons.com",
+            "s.yimg.com",
+            "d32r1sh890xpii.cloudfront.net",
+            "www.orlandosentinel.com",
+            "www.nj.com",
+        ]
+    }
+};
 
 export default nextConfig;
